597c7b0533a465b077fba871f5c94019c902c348
Paulo Santos <paulorfsantos17@gmail.com>
2023-09-12 09:52:25 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./componets/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello word</h1>
+      <Button title="Click Me" />
+    </>
+  );
+}
+
+export default App;
